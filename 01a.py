8633709f372e015a0ced40999c82fe4b0e757f16
0x28F4 <robert_hecht@outlook.de>
2021-12-01 12:24:13 +0100

    day 1 part 1

--- 01a.py
+++ 01a.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
-
-print("hello")
+with open('01.input', 'r') as f:
+    depths = [int(l) for l in f.readlines()]
+    print(sum([depths[i] > depths[i-1] for i in range(1, len(depths))]))
