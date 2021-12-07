8898da3270e43e41a1e5f8d978ef3da9bccd5b94
0x28F4 <robert_hecht@outlook.de>
2021-12-07 09:16:44 +0100

    day 7 part 1

---
+++ 07a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+with open('07.input', 'r') as f:
+    h = np.array([int(h) for h in f.read().split(',')])
+    print(
+        np.min(np.sum(np.array(([np.abs(h - mid) for mid in range(np.min(h),  np.max(h)+1)])), axis=1))
+    )
+
